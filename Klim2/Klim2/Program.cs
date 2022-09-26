dc84876b6af5b10e57955a3d6576fbe9bd3e44a7
KMMatveev <113309637+KMMatveev@users.noreply.github.com>
2022-09-26 11:46:43 +0300

    Add files via upload

---
+++ Klim2/Klim2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+int i;
+double x, k, z;
+x = double.Parse(Console.ReadLine());
+k = 10;
+i = 1;
+z = 1;
+while (i < 10)
+{
+    z *= x;
+    k += z * (10 - i);
+    i++;
+}
+Console.WriteLine(k);
+i = 0;
+z = 1;
+k = 0;
+while (i < 10)
+{
+    z *= x;
+    k += Math.Cos(z);
+    i++;
+}
+Console.WriteLine(k);
+i= int.Parse(Console.ReadLine());
+z = Math.Sin(x);
+double s;
+s = 0;
+k=1;
+while (k < i)
+{
+    s += z;
+    z = Math.Sin(z);
+    k++;
+}
+Console.WriteLine(s);
+
+
+
+Console.ReadKey();
